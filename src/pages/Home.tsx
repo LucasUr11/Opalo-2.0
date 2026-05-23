c147de016128bd55dff2ae0f04d124c92effa315
Lucas Urquiza <lucasestudios2021@gmail.com>
2026-05-23 18:00:16 -0300

    Prueba de optimización sin el HomeCatagories.-

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import { Hero } from "../components/home/Hero";
 import { BrandStory } from "../components/home/BrandStory";
-import { HomeCategories } from "../components/home/HomeCategories";
+// import { HomeCategories } from "../components/home/HomeCategories";
 import { FeaturedProducts } from "../components/home/FeaturedProducts";
 
 export const Home = () => {
     return (
         <>
             <Hero />
-            <HomeCategories />
+            {/* <HomeCategories /> */}
             <FeaturedProducts />
             <BrandStory />
         </>
